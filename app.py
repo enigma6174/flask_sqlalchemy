a2b98511d2fed9771fe514175c84ccbe7f2da53d
enigma6174 <enigma6174@outlook.com>
2021-04-13 23:48:14 +0530

    Updated the environment variable name in app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 
 # Configurations for SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # JWT secret key
